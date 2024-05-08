e351a36aed83b3b6317de69c239934d60cf9898a
Anderson Silva <anderson.ssilva@outlook.com>
2024-05-07 21:36:30 -0300

    versionamento atividade

--- src/unidade_1/Guitarra.java
+++ src/unidade_1/Guitarra.java
@@ -24,5 +24,49 @@ public String getNumeroSerie() {
 public void setNumeroSerie(String numeroSerie) {
 	this.numeroSerie = numeroSerie;
 }
+public String getFabricante() {
+	return fabricante;
+}
+public void setFabricante(String fabricante) {
+	this.fabricante = fabricante;
+}
+public String getModelo() {
+	return modelo;
+}
+public void setModelo(String modelo) {
+	this.modelo = modelo;
+}
+public String getTipo() {
+	return tipo;
+}
+public void setTipo(String tipo) {
+	this.tipo = tipo;
+}
+public String getMadeira() {
+	return madeira;
+}
+public void setMadeira(String madeira) {
+	this.madeira = madeira;
+}
+public double getPreco() {
+	return preco;
+}
+public void setPreco(double preco) {
+	this.preco = preco;
+}
 
+//o método main() é o metodo principal da classe
+public static void main(String[] args) {
+	//instanciamos um objeto chamado "minhaGuitarra" que será do tipo "Guitarra"
+	//Os valores passados são usados pelo construtor da classe para criar
+	//o objeto
+	Guitarra minhaGuitarra = new Guitarra("01020304", "fender", "telecaster", "eletrica", "mogno", 1500);
+	
+//Testando os dados da classe, imprimindo a saída no terminal
+	System.out.println(minhaGuitarra.getNumeroSerie());
+	System.out.println(minhaGuitarra.getMadeira());
+	System.out.println(minhaGuitarra.getPreco());
+	System.out.println(minhaGuitarra.getTipo());
+			
+	}
 }
